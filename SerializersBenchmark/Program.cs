51003d2193dc6c9b10f7a3f055851a253fd98898
Aliaksandr Kukrash <multiarc@gmail.com>
2024-05-04 19:44:11 +0400

    Run target benchmarks in parallel

--- SerializersBenchmark/Program.cs
+++ SerializersBenchmark/Program.cs
@@ -6,7 +6,9 @@ public class Program
 {
     static void Main(string[] args)
     {
-        //dotnet run -c Release -f net8.0 -- --runtimes net48 net6.0 net8.0 --filter *
+        //dotnet run -c Release -f net8.0 -- --runtimes net8.0 --filter *
+        //dotnet run -c Release -f net6.0 -- --runtimes net6.0 --filter *
+        //dotnet run -c Release -f net48 -- --runtimes net48 --filter *
         BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args);
     }
 }
