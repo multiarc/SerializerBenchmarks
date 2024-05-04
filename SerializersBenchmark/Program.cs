de621a57ce6ccae1be562a478740e9bb946ac2c9
Aliaksandr Kukrash <multiarc@gmail.com>
2024-05-04 18:45:18 +0400

    Fix filter

--- SerializersBenchmark/Program.cs
+++ SerializersBenchmark/Program.cs
@@ -6,7 +6,7 @@ public class Program
 {
     static void Main(string[] args)
     {
-        //dotnet run -c Release -f net8.0 -- --runtimes net48 net6.0 net8.0 --filter *
+        //dotnet run -c Release -f net8.0 -- --runtimes net48 net6.0 net8.0 --filter *Benchmarks*
         BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args);
     }
 }
